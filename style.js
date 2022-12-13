af741e0e0d22b2b0bf5c8311d76dbbff03ed8444
Ahmad B <ahmadbello2011@gmail.com>
2022-12-13 10:18:24 -0800

    table

---
+++ style.js
@@ -0,0 +1,45 @@
+
+function sub(){
+   let r =22;
+   let t =6;
+  let u = r-t
+return u
+}
+
+console.log(sub())
+
+function div(){
+     let r =36;
+     let t =6;
+    let u = r/t
+ return u
+ }
+ 
+ console.log(div())
+
+ function multi(){
+    let q =122;
+    let d =44;
+   let y = q*d
+return y
+}
+
+console.log(multi())
+
+function expo(){
+    let h =34;
+    let l =4;
+   let g = h**l
+return g
+}
+
+console.log(expo())
+
+function mod(){
+    let o =672;
+    let k =354;
+   let w = o%k
+return w
+}
+
+console.log(mod())
